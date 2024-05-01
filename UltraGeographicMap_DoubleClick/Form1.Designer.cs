d6e96fbaa5fa64a4fb9a24338143e409f5484456
mkamiishi <mkamiishi@infragistics.com>
2024-05-01 11:45:09 +0900

    Initial commit
    初回コミット

---
+++ UltraGeographicMap_DoubleClick/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace UltraGeographicMap_DoubleClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ultraGeographicMap1 = new Infragistics.Win.DataVisualization.UltraGeographicMap();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGeographicMap1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ultraGeographicMap1
+            // 
+            this.ultraGeographicMap1.BackColor = System.Drawing.Color.White;
+            this.ultraGeographicMap1.CrosshairPoint = new Infragistics.Win.DataVisualization.Point(double.NaN, double.NaN);
+            this.ultraGeographicMap1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ultraGeographicMap1.HorizontalViewScrollbarCornerRadius = double.NaN;
+            this.ultraGeographicMap1.HorizontalViewScrollbarHeight = double.NaN;
+            this.ultraGeographicMap1.HorizontalViewScrollbarInset = double.NaN;
+            this.ultraGeographicMap1.HorizontalViewScrollbarMaxOpacity = double.NaN;
+            this.ultraGeographicMap1.HorizontalViewScrollbarShouldAddAutoTrackInsets = true;
+            this.ultraGeographicMap1.HorizontalViewScrollbarStrokeThickness = double.NaN;
+            this.ultraGeographicMap1.HorizontalViewScrollbarTrackEndInset = double.NaN;
+            this.ultraGeographicMap1.HorizontalViewScrollbarTrackStartInset = double.NaN;
+            this.ultraGeographicMap1.Location = new System.Drawing.Point(0, 0);
+            this.ultraGeographicMap1.Name = "ultraGeographicMap1";
+            this.ultraGeographicMap1.ScrollbarsAnimationDuration = 300;
+            this.ultraGeographicMap1.Size = new System.Drawing.Size(714, 510);
+            this.ultraGeographicMap1.TabIndex = 0;
+            this.ultraGeographicMap1.Text = "ultraGeographicMap1";
+            this.ultraGeographicMap1.VerticalViewScrollbarCornerRadius = double.NaN;
+            this.ultraGeographicMap1.VerticalViewScrollbarInset = double.NaN;
+            this.ultraGeographicMap1.VerticalViewScrollbarMaxOpacity = double.NaN;
+            this.ultraGeographicMap1.VerticalViewScrollbarShouldAddAutoTrackInsets = true;
+            this.ultraGeographicMap1.VerticalViewScrollbarStrokeThickness = double.NaN;
+            this.ultraGeographicMap1.VerticalViewScrollbarTrackEndInset = double.NaN;
+            this.ultraGeographicMap1.VerticalViewScrollbarTrackStartInset = double.NaN;
+            this.ultraGeographicMap1.VerticalViewScrollbarWidth = double.NaN;
+            this.ultraGeographicMap1.WindowRectMinWidth = 5E-06D;
+            this.ultraGeographicMap1.WorldRect = new Infragistics.Win.DataVisualization.Rectangle(-180D, -85.05112878D, 360D, 170.10225756D);
+            this.ultraGeographicMap1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ultraGeographicMap1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 510);
+            this.Controls.Add(this.ultraGeographicMap1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGeographicMap1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.DataVisualization.UltraGeographicMap ultraGeographicMap1;
+    }
+}
+
---
+++ UltraGeographicMap_DoubleClick/Form1.cs
@@ -0,0 +1,52 @@
+﻿using Infragistics.Graphics;
+using Infragistics.Portable.Components.Data;
+using Infragistics.Win.DataVisualization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UltraGeographicMap_DoubleClick
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ultraGeographicMap1.Zoomable = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //緯度35.68095、経度139.76712の地点を中心としてマップをズームする
+            Infragistics.Win.DataVisualization.Rectangle geoRect = geographicFromCentered(35.68095, 139.76712, 0.01);
+            ultraGeographicMap1.ZoomToGeographic(geoRect);
+        }
+
+        private void ultraGeographicMap1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Infragistics.Win.DataVisualization.Point pt = ultraGeographicMap1.GetGeographicPoint(e.Location);
+            MessageBox.Show("緯度:" + pt.Y + ", 経度:" + pt.X);
+        }
+
+        private Infragistics.Win.DataVisualization.Rectangle geographicFromCentered(double latitude, double longitude, double radius)
+        {
+            Infragistics.Win.DataVisualization.Rectangle rect = new Infragistics.Win.DataVisualization.Rectangle()
+            {
+                X = longitude - radius,
+                Y = latitude - radius,
+                Width = radius * 2,
+                Height = radius * 2,
+            };
+
+            return rect;
+        }
+    }
+}
